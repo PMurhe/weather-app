e41a27c5600c47ced6a494b4af658bd899b0deba
Pratik Murhe <PMurhe@chenoainc.com>
2020-07-26 22:07:51 +0530

    Additional Weather Information Added

--- src/utils/weather-stack-api-calls.js
+++ src/utils/weather-stack-api-calls.js
@@ -25,7 +25,6 @@ module.exports.weatherApiCall = function (location) {
 module.exports.forecastApiCall = function (location) {
   return new Promise((resolve, reject) => {
     let apiURL = `http://api.weatherstack.com/forecast?access_key=${config.WEATHER_STACK_ACCESS_KEY}&query=${location}`;
-
     request(
       {
         url: apiURL,
@@ -37,7 +36,12 @@ module.exports.forecastApiCall = function (location) {
         } else {
           if (!response.body.error) {
             const body = response.body;
-            const forecastResponse = `It is currently ${body.current.temperature}  degress out. There is a ${body.current.precip} % chance of rain.`;
+            const forecastResponse = {
+              summary : `${body.current.weather_descriptions[0]}.`,
+              actualTemperature : `It is currently ${body.current.temperature} degress out.`,
+              feelsLike : `It feels ${body.current.feelslike} degrees.`,
+              precip : `There is a ${body.current.precip}% chance of rain.`
+            };
             return resolve({forecast: forecastResponse, location : body.location});
           } else {
             return reject(response.body.error);
